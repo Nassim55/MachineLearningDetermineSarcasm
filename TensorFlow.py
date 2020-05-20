e6d11066862ab336693af481eac6d4353d4de46b
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-05-20 01:05:46 +0100

    20/05/2020 Commit 1

--- TensorFlow.py
+++ TensorFlow.py
@@ -1,92 +1,100 @@
+import json
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-import json
+from flask import Flask
 
-vocab_size = 10000
-embedding_dim = 16
-max_length = 100
-trunc_type = 'post'
-padding_type = 'post'
-oov_tok = "<OOV>"
-training_size = 20000
+app = Flask(__name__)
 
-with open('Sarcasm_Headlines_Dataset.json', 'r') as f:
-    datastore = json.loads("[" + 
-        f.read().replace("}\n{", "},\n{") + 
-    "]")
+@app.route('/')
+def sarcasm_machine_learning():
+    vocab_size = 10000
+    embedding_dim = 16
+    max_length = 100
+    trunc_type = 'post'
+    padding_type = 'post'
+    oov_tok = "<OOV>"
+    training_size = 20000
 
-sentences = []
-labels = []
-for item in datastore:
-    sentences.append(item['headline'])
-    labels.append(item['is_sarcastic'])
+    with open('Sarcasm_Headlines_Dataset.json', 'r') as f:
+        datastore = json.loads("[" + 
+            f.read().replace("}\n{", "},\n{") + 
+        "]")
 
-training_sentences = sentences[0:training_size]
-testing_sentences = sentences[training_size:]
-training_labels = labels[0:training_size]
-testing_labels = labels[training_size:]
+    sentences = []
+    labels = []
+    for item in datastore:
+        sentences.append(item['headline'])
+        labels.append(item['is_sarcastic'])
 
-tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
-tokenizer.fit_on_texts(training_sentences)
+    training_sentences = sentences[0:training_size]
+    testing_sentences = sentences[training_size:]
+    training_labels = labels[0:training_size]
+    testing_labels = labels[training_size:]
 
-word_index = tokenizer.word_index
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(training_sentences)
 
-training_sequences = tokenizer.texts_to_sequences(training_sentences)
-training_padded = pad_sequences(training_sequences, maxlen=max_length,
-    padding=padding_type, truncating=trunc_type)
-testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
-testing_padded = pad_sequences(testing_sequences, maxlen=max_length,
-    padding=padding_type, truncating=trunc_type)
+    word_index = tokenizer.word_index
 
-training_padded = np.array(training_padded)
-training_labels = np.array(training_labels)
-testing_padded = np.array(testing_padded)
-testing_labels = np.array(testing_labels)
+    training_sequences = tokenizer.texts_to_sequences(training_sentences)
+    training_padded = pad_sequences(training_sequences, maxlen=max_length,
+        padding=padding_type, truncating=trunc_type)
+    testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+    testing_padded = pad_sequences(testing_sequences, maxlen=max_length,
+        padding=padding_type, truncating=trunc_type)
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
-    tf.keras.layers.GlobalAveragePooling1D(),
-    tf.keras.layers.Dense(24, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    training_padded = np.array(training_padded)
+    training_labels = np.array(training_labels)
+    testing_padded = np.array(testing_padded)
+    testing_labels = np.array(testing_labels)
 
-num_epochs = 30
-history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_batch_size=(testing_padded, testing_labels), verbose=2)
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+        tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.Dense(24, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-def plot_graphs(history, string):
-    plt.plot(history.history[string])
-    plt.plot(history.history['val_' + string])
-    plt.xlabel('Epochs')
-    plt.ylabel(string)
-    plt.legend([string, 'val_' + string])
-    plt.show()
-  
-#plot_graphs(history, 'accuracy')
-#plot_graphs(history, 'loss')
+    num_epochs = 30
+    history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_batch_size=(testing_padded, testing_labels), verbose=2)
 
+    def plot_graphs(history, string):
+        plt.plot(history.history[string])
+        plt.plot(history.history['val_' + string])
+        plt.xlabel('Epochs')
+        plt.ylabel(string)
+        plt.legend([string, 'val_' + string])
+        plt.show()
 
+    sentence = [
+        'granny starting to fear spiders in the garden might be real',
+        'the weather today is bright and sunny',
+        'oh, yeah right',
+        'of course you did',
+        'During the course of the day you did',
+        'Did you really',
+        'I bet you did',
+        'postman fears local dog'
+    ]
+    sequences = tokenizer.texts_to_sequences(sentence)
+    padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
 
-sentence = [
-    'granny starting to fear spiders in the garden might be real',
-    'the weather today is bright and sunny',
-    'oh, yeah right',
-    'of course you did',
-    'During the course of the day you did',
-]
-sequences = tokenizer.texts_to_sequences(sentence)
-padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+    sarcastic_value = model.predict(padded)
+    is_sentence_sarcastic = []
+    for i in sarcastic_value:
+        if i > 0.5:
+            is_sentence_sarcastic.append('Sarcastic,  Percentage: ' + str(i))
+        else:
+            is_sentence_sarcastic.append('Not sarcastic, Percentage: ' + str(i))
 
-sarcastic_value = model.predict(padded)
-is_sentence_sarcastic = []
-for i in sarcastic_value:
-    if i > 0.5:
-        is_sentence_sarcastic.append('Sarcastic, Value:' + str(i))
-    else:
-        is_sentence_sarcastic.append('Not sarcastic, Value:' + str(i))
+    dict_is_sentence_sarcastic = { i : is_sentence_sarcastic[i] for i in range(0, len(is_sentence_sarcastic))}
 
-print(is_sentence_sarcastic)
+    print(dict_is_sentence_sarcastic)
+    return dict_is_sentence_sarcastic
+
+app.run(port=5000)
