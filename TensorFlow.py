f1b64e65190a0aef61e4562abd9069a9f4edfd67
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-05-22 02:31:23 +0100

    22/05/2020 Commit 1

--- TensorFlow.py
+++ TensorFlow.py
@@ -5,72 +5,65 @@ import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+
+vocab_size = 10000
+embedding_dim = 16
+max_length = 100
+trunc_type = 'post'
+padding_type = 'post'
+oov_tok = "<OOV>"
+training_size = 20000
+
+with open('Sarcasm_Headlines_Dataset.json', 'r') as f:
+    datastore = json.loads("[" + 
+        f.read().replace("}\n{", "},\n{") + 
+    "]")
+
+sentences = []
+labels = []
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+
+word_index = tokenizer.word_index
+
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length,
+    padding=padding_type, truncating=trunc_type)
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length,
+    padding=padding_type, truncating=trunc_type)
+
+training_padded = np.array(training_padded)
+training_labels = np.array(training_labels)
+testing_padded = np.array(testing_padded)
+testing_labels = np.array(testing_labels)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+num_epochs = 30
+history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_batch_size=(testing_padded, testing_labels), verbose=2)
+
+@app.route('/ml')
 def sarcasm_machine_learning():
-    vocab_size = 10000
-    embedding_dim = 16
-    max_length = 100
-    trunc_type = 'post'
-    padding_type = 'post'
-    oov_tok = "<OOV>"
-    training_size = 20000
-
-    with open('Sarcasm_Headlines_Dataset.json', 'r') as f:
-        datastore = json.loads("[" + 
-            f.read().replace("}\n{", "},\n{") + 
-        "]")
-
-    sentences = []
-    labels = []
-    for item in datastore:
-        sentences.append(item['headline'])
-        labels.append(item['is_sarcastic'])
-
-    training_sentences = sentences[0:training_size]
-    testing_sentences = sentences[training_size:]
-    training_labels = labels[0:training_size]
-    testing_labels = labels[training_size:]
-
-    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
-    tokenizer.fit_on_texts(training_sentences)
-
-    word_index = tokenizer.word_index
-
-    training_sequences = tokenizer.texts_to_sequences(training_sentences)
-    training_padded = pad_sequences(training_sequences, maxlen=max_length,
-        padding=padding_type, truncating=trunc_type)
-    testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
-    testing_padded = pad_sequences(testing_sequences, maxlen=max_length,
-        padding=padding_type, truncating=trunc_type)
-
-    training_padded = np.array(training_padded)
-    training_labels = np.array(training_labels)
-    testing_padded = np.array(testing_padded)
-    testing_labels = np.array(testing_labels)
-
-    model = tf.keras.Sequential([
-        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
-        tf.keras.layers.GlobalAveragePooling1D(),
-        tf.keras.layers.Dense(24, activation='relu'),
-        tf.keras.layers.Dense(1, activation='sigmoid')
-    ])
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    num_epochs = 30
-    history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_batch_size=(testing_padded, testing_labels), verbose=2)
-
-    def plot_graphs(history, string):
-        plt.plot(history.history[string])
-        plt.plot(history.history['val_' + string])
-        plt.xlabel('Epochs')
-        plt.ylabel(string)
-        plt.legend([string, 'val_' + string])
-        plt.show()
-
     sentence = [
         'granny starting to fear spiders in the garden might be real',
         'the weather today is bright and sunny',
@@ -92,9 +85,6 @@ def sarcasm_machine_learning():
         else:
             is_sentence_sarcastic.append('Not sarcastic, Percentage: ' + str(i))
 
-    dict_is_sentence_sarcastic = { i : is_sentence_sarcastic[i] for i in range(0, len(is_sentence_sarcastic))}
-
-    print(dict_is_sentence_sarcastic)
-    return dict_is_sentence_sarcastic
+    return jsonify({'sentences': is_sentence_sarcastic})
 
 app.run(port=5000)
